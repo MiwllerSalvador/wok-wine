a8406400966a2928641bf596f5fcf044bf5a95ba
miwllersalvador <m.salvador.barbosa@gmail.com>
2015-11-19 01:49:16 +0000

    trabajando perfiles

--- Development/restapp-sprint1/app/models/user.rb
+++ Development/restapp-sprint1/app/models/user.rb
@@ -1,44 +1,42 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+class User < ActiveRecord::Base 
 
-  has_one :profile
+  # Include default devise modules. Others available are: 
 
-  enum role: [:user, :operator, :chef, :admin]
-  
-  after_initialize :set_default_role
+  # :confirmable, :lockable, :timeoutable and :omniauthable 
 
+  devise :database_authenticatable, :registerable,         
 
-  def set_default_role
+  :recoverable, :rememberable, :trackable, :validatable   
 
-    self.role ||= :user
+  has_one :profile   
+
+  enum role: [:user, :operator, :chef, :admin]   
+
+  after_initialize :set_default_role 
+
+  after_create :set_default_profile   
+
+  def set_default_role   
+
+    self.role ||= :user 
+
+  end      
+
+  def set_default_profile   
+
+    profile = Profile.new   
+
+    profile.user = self   
+
+    profile.save 
 
   end
-  
-  def friendly_name
+ 
 
-    if self.default_profile.first_name.nil?
-      self.email
-    else
-      self.profile.full_name
-    end
+  def friendly_name   
 
-  end
+    self.profile.first_name.nil? ? self.email : self.profile.full_name 
 
-  def default_profile
-
-    if self.profile.nil?
-      profile = Profile.new
-      profile.user = self
-      profile.save
-      self.save      
-    end
-
-  self.profile
+  end  
 
 end
-  
-end
-
--- Development/restapp-sprint1/db/schema.rb
+++ Development/restapp-sprint1/db/schema.rb
@@ -11,15 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105013507) do
+ActiveRecord::Schema.define(version: 20151119014317) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "category_id", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "first_name", limit: 255
-    t.string   "last_name",  limit: 255
+    t.string   "first_name",          limit: 255
+    t.string   "last_name",           limit: 255
     t.datetime "born_at"
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "user_id",             limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
@@ -43,5 +64,6 @@ ActiveRecord::Schema.define(version: 20151105013507) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "products", "categories"
   add_foreign_key "profiles", "users"
 end
