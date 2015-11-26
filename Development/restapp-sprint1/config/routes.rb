84efc6a6a498d6e32c0b535f3ad0df806cec7f57
miwllersalvador <m.salvador.barbosa@gmail.com>
2015-11-26 01:50:22 +0000

    tarea de clase semifinal

--- Development/restapp-sprint1/config/routes.rb
+++ Development/restapp-sprint1/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'menu/index'
+
   resources :profiles, only: [:edit, :update]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  
+  root 'pages#index'
+    
   get 'pages/index'
 
   get 'pages/about_us'
@@ -9,10 +14,13 @@ Rails.application.routes.draw do
   get 'pages/faq'
 
   get 'pages/tos'
+  
 
   get 'pages/contact_us'
-
-  root 'pages#index'
+  
+  get 'menu/index'
+  #get 'menu/category/:id' => "menu#category", as: :menu_category
+  get 'menu/category/:id', controller: :menu, action: :category, as: :menu_category
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
